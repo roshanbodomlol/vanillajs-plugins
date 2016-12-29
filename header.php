84e85e161b9effc6446f620f57718d5cb4047e06
Roshan Maharjan <roshan.bodom@gmail.com>
2016-12-29 13:16:36 +0545

    Starter

---
+++ header.php
@@ -0,0 +1,28 @@
+<!DOCTYPE HTML>
+
+<html>
+<head>
+    	
+    <title> Animal Nepal </title>
+
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+
+    <!--FONTS-->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+    <link rel="stylesheet" href="css/global.css">
+    <link rel="stylesheet" href="css/responsive.css">
+
+</head>
+    
+<body>
+    	
+    <div id="wrapper">
+
+        <div id="header-wrapper">
+            Header
+        </div> <!--Header Wrapper Ends-->
+
+        <div id="content-wrapper">
---
+++ index.php
@@ -0,0 +1,7 @@
+<?php include('header.php'); ?>
+
+    Content
+
+<?php include('footer.php'); ?>
+         
+
