166710f84d8d2a1eb08ac76c7e765cf0092523f1
Roshan Maharjan <roshan.bodom@gmail.com>
2016-12-29 13:14:40 +0545

    Starter

---
+++ footer.php
@@ -0,0 +1,13 @@
+</div>  <!--Content Wrapper-->
+
+		<div id="footer-wrapper">
+
+        </div>  <!--Footer Wrapper Ends-->
+
+		</div> <!--Main Wrapper Ends-->
+
+		<script src="js/thescripts.js"></script>
+        
+    </body>
+    
+</html>
